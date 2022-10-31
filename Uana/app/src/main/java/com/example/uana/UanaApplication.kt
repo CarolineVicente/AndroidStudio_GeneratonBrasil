ec18522eca8fe0a8dd91337d30634ff0738e64de
CarolineVicente <vicente.caroline@hotmail.com>
2022-10-31 13:38:22 -0300

    ajuste

--- Uana/app/src/main/java/com/example/uana/UanaApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.example.uana
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class UanaApplication: Application() {
-}
--- Uana/app/src/main/java/com/example/uana/util/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.example.uana.util
-
-object Constants {
-    const val BASE_URL = "https://projetouana.onrender.com/"
-}
